3213d71a793b64612e8ae0d42c811e4fe9201840
cc <cczjum@outlook.com>
2024-04-01 18:38:28 +0800

    增加IntAddress类来封装socket_addr

---
+++ 03/InetAddress.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+class InetAddress {
+private:
+    sockaddr_in addr_;
+public:
+    InetAddress(uint16_t port);
+    InetAddress(const std::string &ip, uint16_t port);
+    InetAddress(const sockaddr_in &addr): addr_(addr) {}
+    ~InetAddress();
+
+    const char* ip() const; // 返回点分十进制的ip地址
+    uint16_t port() const; // 返回端口号
+    const sockaddr* addr() const; // 返回sockaddr结构体
+};
