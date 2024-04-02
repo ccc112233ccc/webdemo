3a19881760ba1bcdf1c4759aafa741833aa9aea5
cc <cczjum@outlook.com>
2024-04-02 10:34:09 +0800

    封装epoll操作添加Epoll类

---
+++ 05/Epoll.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/epoll.h>
+#include<sys/types.h>
+#include<vector>
+#include<string.h>
+
+
+class Epoll {
+private:
+    static const int MAX_EVENTS_ = 100;
+    int epoll_fd_;
+    epoll_event events_[MAX_EVENTS_];
+
+public:
+    Epoll();
+    ~Epoll();
+
+    void add_fd(int fd, uint32_t op);
+    std::vector<epoll_event> wait();
+};
---
+++ 05/Socket.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/tcp.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include"InetAddress.h"
+
+int createNonblockingSocket(); // 创建一个非阻塞的套接字
+
+class Socket {
+private:
+    const int fd_;
+public:
+    Socket(int fd) : fd_(fd) {}
+    ~Socket();
+    int fd() const { return fd_; }
+
+    void setReuseAddr(bool on); // 设置地址复用
+    void setReusePort(bool on); // 设置端口复用
+    void setKeepAlive(bool on); // 设置keepalive
+    void setNoDelay(bool on); // 设置Nagle算法
+
+    void bindAddress(const InetAddress &addr); // 绑定地址
+    void listen(int n = 128); // 监听
+    int accept(InetAddress &peeraddr); // 接受连接
+};
