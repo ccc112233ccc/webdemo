b31a6a2b1014018015d05e5f193106e378719535
cc <cczjum@outlook.com>
2024-04-05 16:45:19 +0800

    将new_connection函数挪到Acceptor中

---
+++ 14/Acceptor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <functional>
+
+#include "Channel.h"
+#include "EventLoop.h"
+#include "InetAddress.h"
+#include "Socket.h"
+
+class Acceptor {
+ private:
+  EventLoop* loop_;
+  Socket* servsock_;
+  Channel* servch_;
+
+ public:
+  Acceptor(EventLoop* loop, int port);
+  ~Acceptor();
+  void new_connect();
+};
