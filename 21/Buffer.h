0dd3add3f33a4bd96bee5b962a8dc21d3e050443
cc <cczjum@outlook.com>
2024-04-06 15:03:38 +0800

    使用写事件回调函数，让服务器的输出缓冲区写回去

---
+++ 21/Buffer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Buffer {
+ private:
+  std::string buffer_;
+
+ public:
+  Buffer();
+  ~Buffer();
+
+  void append(const char* data, size_t len);
+  void erase(size_t pos, size_t len);
+  size_t size();
+  const char* data();
+  void clear();
+};
---
+++ 21/Connection.cpp
@@ -0,0 +1,96 @@
+#include "Connection.h"
+
+Connection::Connection(EventLoop* loop, Socket* clientsock)
+    : loop_(loop), clientsock_(clientsock) {
+  clientch_ = new Channel(loop_, clientsock_->fd());
+  clientch_->set_read_callback(std::bind(&Connection::on_message, this));
+  clientch_->set_close_callback(std::bind(&Connection::close_callback, this));
+  clientch_->set_error_callback(std::bind(&Connection::error_callback, this));
+  clientch_->set_write_callback(std::bind(&Connection::write_callback, this));
+  clientch_->useet();
+  clientch_->enable_reading();
+}
+
+Connection::~Connection() {
+  delete clientsock_;
+  delete clientch_;
+}
+
+void Connection::close_callback() { close_callback_(this); }
+
+void Connection::error_callback() { error_callback_(this); }
+void Connection::write_callback() {
+  if (output_buffer_.size() > 0) {
+    int n = ::send(fd(), output_buffer_.data(), output_buffer_.size(), 0);
+    if (n == -1) {
+      perror("send");
+    } else {
+      output_buffer_.erase(0, n);
+    }
+  }
+  if (output_buffer_.size() == 0) {
+    clientch_->disable_writing();
+  }
+}
+
+void Connection::on_message() {
+  char buf[1024];
+  while (true) {
+    memset(buf, 0, sizeof(buf));
+    int n = recv(fd(), buf, sizeof(buf), 0);
+    if (n == -1) {
+      if (errno == EAGAIN) {
+        // printf("recv : %s\n", input_buffer_.data());
+        while (true) {
+          int len;
+          memcpy(&len, input_buffer_.data(), sizeof(len));
+          if (input_buffer_.size() < sizeof(len) + len) {
+            // 说明数据还没接收完
+            break;
+          }
+          std::string message(input_buffer_.data() + sizeof(len),
+                              len);  // 从 input_buffer_ 中提取出一个完整的消息
+          input_buffer_.erase(
+              0, sizeof(len) + len);  // 从 input_buffer_ 中删除这个消息
+
+          printf("recv : %s\n", message.c_str());
+
+          // message = "hello, " + message;
+          // len = message.size();
+          // std::string output_buffer(sizeof(len), 0);
+          // memcpy(&output_buffer[0], &len, sizeof(len));
+          // output_buffer.append(message);
+          // send(fd(), output_buffer.data(), output_buffer.size(), 0);
+          message_callback_(this, message);
+        }
+
+        break;
+      } else {
+        perror("recv");
+        return;
+      }
+    } else if (n == 0) {
+      // 对端关闭连接
+      // printf("对端关闭连接（n = 0）\n");
+      // close(fd_);
+      close_callback();
+      break;
+    } else {
+      input_buffer_.append(buf, n);
+
+      // 返回一个有效的 HTTP 响应
+      // const char* response =
+      //     "HTTP/1.1 200 OK\r\n"
+      //     "Content-Type: text/plain\r\n"
+      //     "Content-Length: 12\r\n"
+      //     "\r\n"
+      //     "Hello, world";
+      // send(fd_, response, strlen(response), 0);
+    }
+  }
+}
+
+void Connection::send(const char* data, size_t len) {
+  output_buffer_.append(data, len);
+  clientch_->enable_writing();
+}
