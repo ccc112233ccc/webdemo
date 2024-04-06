09e8772007cc0ee61ad26820edf12541be45db10
cc <cczjum@outlook.com>
2024-04-06 21:02:16 +0800

    实现主从reactor模型，acceptor运行在主事件循环中，connection运行在从事件循环中。

---
+++ 26/EchoServer.cpp
@@ -0,0 +1,55 @@
+#include "EchoServer.h"
+
+#include <iostream>
+
+EchoServer::EchoServer(uint16_t port, int threadnum)
+    : server_(port, threadnum) {
+  server_.set_new_connect_callback(
+      std::bind(&EchoServer::HandleNewConnection, this, std::placeholders::_1));
+  server_.set_close_connect_callback(std::bind(
+      &EchoServer::HandleCloseConnection, this, std::placeholders::_1));
+  server_.set_error_connect_callback(std::bind(
+      &EchoServer::HandleErrorConnection, this, std::placeholders::_1));
+  server_.set_message_callback(std::bind(&EchoServer::HandleMessage, this,
+                                         std::placeholders::_1,
+                                         std::placeholders::_2));
+  server_.set_complete_callback(
+      std::bind(&EchoServer::HandleComplete, this, std::placeholders::_1));
+  server_.set_timeout_callback(
+      std::bind(&EchoServer::HandleTimeout, this, std::placeholders::_1));
+}
+
+EchoServer::~EchoServer() {}
+
+void EchoServer::start() { server_.start(); }
+
+void EchoServer::HandleNewConnection(Connection* conn) {
+  std::cout << "New connection" << std::endl;
+  printf("new connection(), thread is %ld \n", syscall(SYS_gettid));
+}
+
+void EchoServer::HandleCloseConnection(Connection* conn) {
+  std::cout << "Close connection" << std::endl;
+}
+
+void EchoServer::HandleErrorConnection(Connection* conn) {
+  std::cout << "Error connection" << std::endl;
+}
+
+void EchoServer::HandleMessage(Connection* conn, std::string& message) {
+  printf("message: %s, thread is %ld \n", message.c_str(), syscall(SYS_gettid));
+  message = "reply: " + message;
+  // int len = message.size();
+  // std::string output_buffer((char*)&len, sizeof(len));
+  // output_buffer.append(message);
+  // // send(conn->fd(), output_buffer.data(), output_buffer.size(), 0);
+  conn->send(message.data(), message.size());
+}
+
+void EchoServer::HandleComplete(Connection* conn) {
+  std::cout << "Complete" << std::endl;
+}
+
+void EchoServer::HandleTimeout(EventLoop* loop) {
+  std::cout << "Timeout" << std::endl;
+}
