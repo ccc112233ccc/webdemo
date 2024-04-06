1c5a7672c254af138559a9b8bea9c44e23b90eaa
cc <cczjum@outlook.com>
2024-04-06 17:15:33 +0800

    添加线程池

---
+++ 25/Epoll.cpp
@@ -0,0 +1,52 @@
+#include "Epoll.h"
+
+#include <iostream>
+
+#include "Channel.h"
+
+Epoll::Epoll() {
+  epoll_fd_ = epoll_create(1);
+  if (epoll_fd_ == -1) {
+    perror("epoll_create");
+    exit(1);
+  }
+}
+
+Epoll::~Epoll() { close(epoll_fd_); }
+
+void Epoll::update_channel(Channel* channel) {
+  epoll_event ev;
+  ev.events = channel->events();
+  ev.data.ptr = channel;
+  if (channel->is_in_epoll()) {
+    if (epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, channel->fd(), &ev) == -1) {
+      perror("epoll_ctl");
+      exit(1);
+    }
+    return;
+  } else {
+    if (epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, channel->fd(), &ev) == -1) {
+      perror("epoll_ctl");
+      exit(1);
+    }
+    channel->setin_epoll();
+  }
+}
+std::vector<Channel*> Epoll::wait(int timeout) {
+  int nfds = epoll_wait(epoll_fd_, events_, MAX_EVENTS_, timeout);
+  if (nfds == -1) {
+    perror("epoll_wait");
+    exit(1);
+  }
+  if (nfds == 0) {
+    // 超时
+    return std::vector<Channel*>();
+  }
+  std::vector<Channel*> ret;
+  for (int i = 0; i < nfds; i++) {
+    Channel* channel = (Channel*)events_[i].data.ptr;
+    channel->set_revents(events_[i].events);
+    ret.push_back(channel);
+  }
+  return ret;
+}
